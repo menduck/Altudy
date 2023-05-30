b1dba0870688d6d50be73ba8655c278661f6d157
thebjko <thebjko@gmail.com>
2023-05-30 16:53:05 +0900

    [feat] markdown to detail.html

--- reviews/templatetags/custom_filters.py
+++ reviews/templatetags/custom_filters.py
@@ -1,4 +1,7 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
+
 
 register = template.Library()
 
@@ -6,6 +9,13 @@ register = template.Library()
 def get_attributes(obj):
     return dir(obj)
 
+
 @register.filter('type')
 def get_attributes(obj):
     return type(obj)
+
+
+@register.filter('markdown')
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
