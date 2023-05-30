fc82c7a2965b2053579ffd5b1b650de4cc88d4f6
thebjko <thebjko@gmail.com>
2023-05-30 16:42:54 +0900

    [feat] add help_text to tags field

--- reviews/fields.py
+++ reviews/fields.py
@@ -1,8 +1,12 @@
+from typing import Any, Optional, Sequence, Type, Union
 from django import forms
+from django.forms.widgets import Widget
+from taggit.forms import TagWidget
 from taggit.utils import parse_tags
 
-
 class SpaceSeparatedTagsField(forms.CharField):
+    widget = TagWidget()
+
     def clean(self, value):
         '''
         space separates tags
