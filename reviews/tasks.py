80e3d91c6f72a08baf1e5dc84c269b837f99208a
thebjko <thebjko@gmail.com>
2023-05-31 10:42:41 +0900

    [feat] delete tasks.py

--- reviews/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-
-@shared_task
-def remove_unused_tags(obj):
-    for tag in obj.tags.prefetch_related('problem_set', 'review_set', 'comment_set'):
-        if tag.problem_set.count() == tag.review_set.count() == tag.comment_set.count() == 0:
-            tag.delete()
