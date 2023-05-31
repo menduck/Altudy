fce6940e48e1bf37b8b0e16bad413a79e329c9f7
ming0830 <minsun0830@naver.com>
2023-05-31 17:48:00 +0900

    [Feat] 최신 스터디 카드 캐러셀 추가

---
+++ static/JS/swiper.js
@@ -0,0 +1,29 @@
+const swiperEl = document.querySelector('swiper-container')
+    Object.assign(swiperEl, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
+    });
+
+    swiperEl.initialize();
