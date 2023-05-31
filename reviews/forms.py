077e6f21d393679da4c3ba946392b0536402f86d
thebjko <thebjko@gmail.com>
2023-05-31 14:10:30 +0900

    [hotfix] tag field

--- reviews/forms.py
+++ reviews/forms.py
@@ -26,7 +26,7 @@ class ProblemForm(forms.ModelForm):
 
 
 class ReviewForm(forms.ModelForm):
-    tags = SpaceSeparatedTagsField(
+    tags = TagField(
         required=False,
         widget=TagWidget,
         help_text = '태그를 입력하세요. 공백문자로 태그를 구분하며 대소문자를 구분하지 않습니다.',
