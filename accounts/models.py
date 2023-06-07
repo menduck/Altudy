7f5e615bbf08732dab8de000e56bf043a6801f87
hyundol2 <nhs503601@gmail.com>
2023-06-07 22:38:01 +0900

    [Feat] accounts - 경험치 모델과 뱃지 추가

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,31 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+def get_emoji(exp):
+    if exp >= 1000:
+        return '🦖'
+    elif exp >= 500:
+        return '🦕'
+    elif exp >= 200:
+        return '🦎'
+    elif exp >= 90:
+        return '🐥'
+    elif exp >= 30:
+        return '🐣'
+    elif exp >= 0:
+        return '🥚'
+    else:
+        return ''
+
+
 class User(AbstractUser):
-    pass
+    experience = models.PositiveIntegerField(default=0)
+
+
+    def emoji_username(self):
+        emoji = get_emoji(self.experience)
+        return emoji + self.username
+
+
+    def emoji(self):
+        return get_emoji(self.experience)
