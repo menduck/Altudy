7efb64475092e738df653c7a4a38bf72d76bb53d
ming0830 <minsun0830@naver.com>
2023-06-07 09:54:55 +0900

    [Fix] 로그아웃 시 alarm 분기점

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,27 +1,30 @@
 from studies.models import Study, Studying, Announcement
 from django.utils import timezone
 from datetime import timedelta
-from django.contrib.auth.decorators import login_required
 
-@login_required
 def alarm(request):
-    lead_studies = Study.objects.filter(user=request.user)
-    all_requests = list()
-    for study in lead_studies:
-        all_requests.append((study, study.join_request.all()))
-    
-    all_announcements = list()
-    studyings = Studying.objects.filter(user=request.user)
-    for studying in studyings:
-        # 최근 일주일 간의 공지만 표시
-        announcements = Announcement.objects.filter(
-            study=studying.study, 
-            updated_at__gte=timezone.now() - timedelta(days=7)
-            )
-        all_announcements.append(announcements)
-    context = {
-        'all_requests': all_requests,
-        'all_announcements': all_announcements,
-    }
+    if request.user.is_authenticated:
+        lead_studies = Study.objects.filter(user=request.user)
+        all_requests = list()
+        for study in lead_studies:
+            all_requests.append((study, study.join_request.all()))
+        
+        all_announcements = list()
+        studyings = Studying.objects.filter(user=request.user)
+        for studying in studyings:
+            # 최근 일주일 간의 공지만 표시
+            announcements = Announcement.objects.filter(
+                study=studying.study, 
+                updated_at__gte=timezone.now() - timedelta(days=7)
+                )
+            all_announcements.append(announcements)
+        context = {
+            'all_requests': all_requests,
+            'all_announcements': all_announcements,
+        }
+    else:
+        context = {
+            
+        }
 
     return context
