72903cb18a6e90bfcb6ff783dc89b1cf073e3cfd
thebjko <thebjko@gmail.com>
2023-06-08 16:11:00 +0900

    [setup] install django-htmx

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_htmx',
     'storages',
     'taggit',
     'multiselectfield',
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
