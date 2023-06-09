eba35fc1f80942e8d17ecbec1347cfd5dcd6c466
ming0830 <minsun0830@naver.com>
2023-06-09 19:07:26 +0900

    [Fix] tostui view 추가, 및 코드문법강조 플러그인 추가

--- reviews/static/reviews/toastui.js
+++ reviews/static/reviews/toastui.js
@@ -2,12 +2,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const editorDiv = document.getElementById('editor');
   const FormTextArea = document.querySelector('textarea')
-  const editor = new toastui.Editor({
+  const { Editor } = toastui;  
+  const { codeSyntaxHighlight } = Editor.plugin;  
+  const editor = new Editor({
     el: editorDiv,
     height: '500px',
     initialEditType: 'markdown',
     previewStyle: 'vertical',
     initialValue: FormTextArea.textContent,
+    plugins: [codeSyntaxHighlight]
   })
   const form = document.getElementById('form')
   form.addEventListener('submit', (e) => {
