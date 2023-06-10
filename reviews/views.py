9de52271af277d1d855ade6bbbf0a18a7e79996d
thebjko <thebjko@gmail.com>
2023-06-10 16:07:46 +0900

    [doc] remove unnecessary comment

--- reviews/views.py
+++ reviews/views.py
@@ -154,7 +154,6 @@ def review_create(request, pk):
             context = {
                 'problem': Problem.objects.get(pk=pk)
             }
-            # return render(request, 'reviews/detail.html', context)
             return redirect('reviews:detail', review.problem.pk)
     else:
         form = ReviewForm()
