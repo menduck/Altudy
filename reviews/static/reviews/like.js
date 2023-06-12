b6d0aa23521b5343bc143190b4aa3d5cb31104fb
thebjko <thebjko@gmail.com>
2023-06-12 17:41:21 +0900

    [refactor] commented out like.js

--- reviews/static/reviews/like.js
+++ reviews/static/reviews/like.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', (x) => {
-  const forms = document.querySelectorAll('#like-forms')
-  const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
+// document.addEventListener('DOMContentLoaded', (x) => {
+//   const forms = document.querySelectorAll('#like-forms')
+//   const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
   
-  forms.forEach((form) => {
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      const objectIdentifier = e.target.dataset.objectIdentifier
-      const endpoint = '/reviews/like/'
-      const data = {'objectIdentifier': objectIdentifier}
-      const headers = {
-        'X-CSRFToken': csrftoken,
-        'Content-Type': 'application/json',
-      }
-      axios.post(endpoint, data, {headers}).then((r) => {
-        swapText = r.data.swap_text
-        const inputEl = document.getElementById(`${objectIdentifier}`)
-        inputEl.value = `${swapText}`
-      }).catch((error) => {
-        console.log(error.response)
-      })
-    })
-  })
-})
+//   forms.forEach((form) => {
+//     form.addEventListener('submit', (e) => {
+//       e.preventDefault()
+//       const objectIdentifier = e.target.dataset.objectIdentifier
+//       const endpoint = '/reviews/like/'
+//       const data = {'objectIdentifier': objectIdentifier}
+//       const headers = {
+//         'X-CSRFToken': csrftoken,
+//         'Content-Type': 'application/json',
+//       }
+//       axios.post(endpoint, data, {headers}).then((r) => {
+//         swapText = r.data.swap_text
+//         const inputEl = document.getElementById(`${objectIdentifier}`)
+//         inputEl.value = `${swapText}`
+//       }).catch((error) => {
+//         console.log(error.response)
+//       })
+//     })
+//   })
+// })
