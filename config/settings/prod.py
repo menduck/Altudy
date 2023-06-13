7a565eea13f00c5cb208c889593f770665d06302
thebjko <thebjko@gmail.com>
2023-06-13 15:29:20 +0900

    [setup] add static root for collectstatic

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -105,3 +105,7 @@ STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.ap-northeast-2.amazonaws.com"
 AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = False
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/ubuntu/Altudy/static'
