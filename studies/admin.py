ba1ef3fa194c46bc771eb1f3ac1c76c4e7a1a7ea
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-06-15 11:02:02 +0900

    [Feat] (studies) admin site 접근 가능 테이블 추가

--- studies/admin.py
+++ studies/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Study, Studying
+from .models import Study, Studying, Announcement, AnnouncementRead, StudyComment
 
 # Register your models here.
 admin.site.register(Study)
 admin.site.register(Studying)
+admin.site.register(Announcement)
+admin.site.register(AnnouncementRead)
+admin.site.register(StudyComment)
